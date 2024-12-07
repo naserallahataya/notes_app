587d99a22cd53878740db7323fbbe11f1c2a74ea
naserallahataya <naserallahataya@gmail.com>
2024-12-08 00:00:58 +0300

    add fields to bottom sheet

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/views/widgets/custom_tet_filed.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        CustomTetFiled(hint: 'Title'),
+        CustomTetFiled(hint: 'Content', maxLines: 5),
+      ],
+    );
   }
 }
