88d3dd446853d34c477fc8f543e9f11405d2d58c
naserallahataya <naserallahataya@gmail.com>
2024-12-07 08:50:19 +0300

    create note list view

---
+++ lib/views/widgets/notes_list_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:note_app/views/widgets/custom_note_item.dart';
+
+class NoteListView extends StatelessWidget {
+  const NoteListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      child: ListView.builder(
+          padding: EdgeInsets.zero,
+          itemBuilder: (context, index) {
+            return const Padding(
+              padding: EdgeInsets.symmetric(vertical: 4.0),
+              child: NoteItem(),
+            );
+          }),
+    );
+  }
+}
