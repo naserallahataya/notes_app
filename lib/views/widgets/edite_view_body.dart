4272a2ca5ea4694a5ca790440979738e41afbdc1
naserallahataya <naserallahataya@gmail.com>
2024-12-08 22:36:38 +0300

    create note model

--- lib/views/widgets/edite_view_body.dart
+++ lib/views/widgets/edite_view_body.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/views/widgets/custom_app_bar.dart';
+import 'package:note_app/views/widgets/custom_tet_filed.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  const Padding(
-      padding:     EdgeInsets.symmetric(horizontal: 24),
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24),
       child: Column(
         children: [
           SizedBox(
@@ -16,7 +17,12 @@ class EditNoteViewBody extends StatelessWidget {
           CustomAppBar(
             title: 'Edit Note',
             icon: Icons.check,
-          )
+          ),
+          CustomTetFiled(hint: 'Title'),
+          CustomTetFiled(
+            hint: 'contant ',
+            maxLines: 5,
+          ),
         ],
       ),
     );
