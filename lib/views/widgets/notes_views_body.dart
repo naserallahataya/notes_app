c7095b456cd456e1c050b3777845fbf660040bf0
nasr <naserallahataya@gmail.com>
2024-12-24 05:35:07 +0300

    display notes part 1

--- lib/views/widgets/notes_views_body.dart
+++ lib/views/widgets/notes_views_body.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:note_app/views/widgets/custom_app_bar.dart';
 import 'package:note_app/views/widgets/notes_list_view.dart';
 
-class NotesViewsBody extends StatelessWidget {
+class NotesViewsBody extends StatefulWidget {
   const NotesViewsBody({super.key});
+
+  @override
+  State<NotesViewsBody> createState() => _NotesViewsBodyState();
+}
+
+class _NotesViewsBodyState extends State<NotesViewsBody> {
+  @override
+  void initState() {
+    BlocProvider.of<NotesCubit>(context).fetchAllNotes();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
