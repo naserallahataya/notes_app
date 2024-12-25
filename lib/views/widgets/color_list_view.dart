1214ecf90d069bd935c4d8e62241e822b1e03a29
nasr <naserallahataya@gmail.com>
2024-12-26 02:59:41 +0300

    colors list view part 2

--- lib/views/widgets/color_list_view.dart
+++ lib/views/widgets/color_list_view.dart
@@ -1,30 +1,66 @@
-
 import 'package:flutter/material.dart';
 
 class ColorItem extends StatelessWidget {
-  const ColorItem({super.key});
+  const ColorItem({super.key, required this.isActinve, required this.color});
+  final bool isActinve;
 
+  final Color color;
   @override
   Widget build(BuildContext context) {
-    return const CircleAvatar(
-      radius: 38,
-      backgroundColor: Colors.blue,
-    );
+    return isActinve
+        ? CircleAvatar(
+            backgroundColor: Colors.white,
+            radius: 34,
+            child: CircleAvatar(
+              radius: 29,
+              backgroundColor: color,
+            ),
+          )
+        : CircleAvatar(
+            radius: 34,
+            backgroundColor: color,
+          );
   }
 }
 
-class ColorLostView extends StatelessWidget {
+class ColorLostView extends StatefulWidget {
   const ColorLostView({super.key});
 
+  @override
+  State<ColorLostView> createState() => _ColorLostViewState();
+}
+
+class _ColorLostViewState extends State<ColorLostView> {
+  int currentIndex = 0;
+
+  List<Color> colors = const [
+    Color(0xff780000),
+    Color(0xffC1121F),
+    Color(0xffFDF0D5),
+    Color(0xff003049),
+    Color(0xff669BBC),
+  ];
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 38 * 2,
       child: ListView.builder(
-        itemCount: 10,
+        itemCount: colors.length,
         scrollDirection: Axis.horizontal,
         itemBuilder: (context, index) {
-          return const ColorItem();
+          return Padding(
+            padding: const EdgeInsets.all(6.0),
+            child: GestureDetector(
+              onTap: () {
+                currentIndex = index;
+                setState(() {});
+              },
+              child: ColorItem(
+                color: colors[index],
+                isActinve: currentIndex == index,
+              ),
+            ),
+          );
         },
       ),
     );
